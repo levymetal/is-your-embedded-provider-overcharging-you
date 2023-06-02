4f2f9c50b3047ebce4d0b5b04f991a4284e50180
levymetal <noreply@levymetal.net>
2023-06-02 15:53:33 +1200

    add gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
         trackingId: 'UA-32030146-2',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          'G-E51S0E1JG0', // Google Analytics / GA
+        ],
+      },
+    },
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
   ],
