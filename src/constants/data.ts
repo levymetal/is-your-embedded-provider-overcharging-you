c4d9d738a500ac51b87c00ad92418a3117a47c9d
levymetal <noreply@levymetal.net>
2023-07-04 14:06:54 +1000

    update pricing data for FY24

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,56 +1,56 @@
 export const PRICING_DATA = {
   ausnet: {
     residential: {
-      supply: 1.2994,
-      usage: 0.2893, // block 1
-      usage2: 0.3070, // block 2
+      supply: 1.3251,
+      usage: 0.3851, // block 1
+      usage2: 0.3955, // block 2
       block1AnnualKWh: 1020 * 4, // up to 1020kWh used in a quarterly period
     },
     business: {
-      supply: 1.2994,
-      usage: 0.3308, // block 1
-      usage2: 0.3663, // block 2
+      supply: 1.3251,
+      usage: 0.4155, // block 1
+      usage2: 0.4381, // block 2
       block1AnnualKWh: 1020 * 4, // up to 1020kWh used in a quarterly period
     },
   },
   citipower: {
     residential: {
-      supply: 1.1623,
-      usage: 0.2170,
+      supply: 1.1525,
+      usage: 0.2871,
     },
     business: {
-      supply: 1.3860,
-      usage: 0.2166,
+      supply: 1.3434,
+      usage: 0.2834,
     },
   },
   jemena: {
     residential: {
-      supply: 1.1577,
-      usage: 0.2322,
+      supply: 1.1438,
+      usage: 0.3252,
     },
     business: {
-      supply: 1.3087,
-      usage: 0.2468,
+      supply: 1.3057,
+      usage: 0.3307,
     },
   },
   powercor: {
     residential: {
-      supply: 1.3102,
-      usage: 0.2333,
+      supply: 1.3002,
+      usage: 0.3292,
     },
     business: {
-      supply: 1.4380,
-      usage: 0.2333,
+      supply: 1.4275,
+      usage: 0.3062,
     },
   },
   united: {
     residential: {
-      supply: 1.0753,
-      usage: 0.2327,
+      supply: 1.0814,
+      usage: 0.3174,
     },
     business: {
-      supply: 1.2191,
-      usage: 0.2279,
+      supply: 1.2405,
+      usage: 0.3028,
     },
   },
 };
